5a90e0af56f75582f1c41f2ab2293232a4770179
Philip Makings <pmakings@salesforce.com>
2016-10-19 21:29:33 +0000

    Update to the Turtle

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 TinyTurtle.apply(window);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
+}
+function triangle(){
+    forward (50);
+    right (120);
+    forward (50);
+    right(120);
+    forward(50);
 }
 
+function shape(size, angle)
+{
+    forward(size);
+    right(angle);
+    forward(size);
+    right(angle);
+    forward(size)    
+    right(angle);
+    forward(size);
+    right(angle);
+}
 // Type your function call below
-
+shape(50,120);
 
 stamp();
